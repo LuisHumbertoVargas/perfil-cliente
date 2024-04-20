2c4902c869a39da8d1539aae657891ef16626bee
Luis <luish.vargas09@gmail.com>
2024-04-20 15:29:51 -0600

    Botón para subir

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ function toggleHistory(button) {
   dataSection.classList.add('hidden');
   historySection.classList.toggle('hidden');
 }
+
+function scrollToTopOrClose() {
+  // Si el scroll actual es mayor a 0, hacemos scroll hacia arriba
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  } else {
+    // Si estamos en la parte superior, cierra el contenido oculto
+    toggleData(document.querySelector('.toggle-btn-up')); // Llama a tu función toggleData o toggleHistory dependiendo de tus necesidades
+  }
+}
